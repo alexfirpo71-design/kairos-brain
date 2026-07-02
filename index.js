1dcee72ce88d19b8c86b37b147b718ebc1ac416e
alexfirpo71-design <alex.firpo71@gmail.com>
2026-07-02 20:04:02 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 export default function handler(req, res) {
-  res.status(200).send('OK');
+  res.status(200).json({ 
+    message: "Ricevuto", 
+    method: req.method,
+    length: req.headers['content-length'] 
+  });
 }
