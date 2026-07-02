cc5e8c41f3db9b678254c8fbbea2aa8906ab2ea5
alexfirpo71-design <alex.firpo71@gmail.com>
2026-07-02 18:21:57 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 export default function handler(req, res) {
-  res.status(200).json({ status: "Ricevuto" });
+  // Ora leggiamo i dati dall'URL (query)
+  const trigger = req.query.trigger;
+  
+  console.log("Dati ricevuti:", trigger);
+  
+  res.status(200).json({ status: "Ricevuto", valore: trigger });
 }
