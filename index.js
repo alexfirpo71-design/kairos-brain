cb4eb5cc1fc752b1bd8199b7ca193c276ee49d00
alexfirpo71-design <alex.firpo71@gmail.com>
2026-07-06 01:21:37 +0200

    Update index.js

--- index.js
+++ index.js
@@ -5,10 +5,8 @@ export const config = {
 };
 
 export default async function handler(req, res) {
-  if (req.method === 'POST') {
-    // Risposta fissa per testare se il server risponde correttamente
-    res.status(200).send("https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3");
-  } else {
-    res.status(405).send("Method not allowed");
-  }
+  // Aggiungiamo un log che apparirà nella dashboard di Vercel
+  console.log("CHIAMATA RICEVUTA!"); 
+  
+  res.status(200).send("https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3");
 }
