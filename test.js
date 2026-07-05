2d4069421fcc75a30f847e081aedf8dc8b7880c8
alexfirpo71-design <alex.firpo71@gmail.com>
2026-07-06 01:34:56 +0200

    Update test.js

--- test.js
+++ test.js
@@ -1,12 +1,3 @@
-export const config = {
-  api: {
-    bodyParser: false,
-  },
-};
-
-export default async function handler(req, res) {
-  // Aggiungiamo un log che apparirà nella dashboard di Vercel
-  console.log("CHIAMATA RICEVUTA!"); 
-  
+export default function handler(req, res) {
   res.status(200).send("https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3");
 }
