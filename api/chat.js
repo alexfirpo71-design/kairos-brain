9e97d5ab331f8432f1132473642a858f47ee7eea
alexfirpo71-design <alex.firpo71@gmail.com>
2026-07-20 19:16:46 +0200

    Update chat.js

--- api/chat.js
+++ api/chat.js
@@ -2,7 +2,7 @@ import fetch from 'node-fetch';
 
 export default async function handler(req, res) {
   if (req.method !== 'POST') {
-    return res.status(405.send('Solo POST'));
+    return res.status(405).send('Solo POST');
   }
 
   try {
