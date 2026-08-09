5ea7f824052cc8756fc188d653cac33f535e9ae1
alexfirpo71-design <alex.firpo71@gmail.com>
2026-08-09 17:20:09 +0200

    Update server.js

--- server.js
+++ server.js
@@ -286,7 +286,7 @@ async function handleCameraTrigger(ws) {
                         content: 'Ho attivato la telecamera.'
                     }
                 ],
-                max_tokens: 150,
+                max_tokens: 1000,
                 temperature: 0.0
             })
         });
