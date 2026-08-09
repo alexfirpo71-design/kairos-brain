d0eec7b70df18eb22279d483d28cbb7875715779
alexfirpo71-design <alex.firpo71@gmail.com>
2026-08-09 13:46:42 +0200

    Update server.js

--- server.js
+++ server.js
@@ -177,7 +177,7 @@ CONTESTO PRIVATO di cui non parlare in continuazione se non richiesto (da usare 
         body: JSON.stringify({
             model: 'llama-3.1-8b-instant',
             messages: messages,
-            max_tokens: 3000,
+            max_tokens: 999,
             temperature: 0.7
         })
     });
