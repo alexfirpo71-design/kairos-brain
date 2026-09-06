6ea90cddf1f77649a4b394c530a6b125169c9a42
alexfirpo71-design <alex.firpo71@gmail.com>
2026-09-06 10:35:08 +0200

    Update server.js

--- server.js
+++ server.js
@@ -102,7 +102,7 @@ async function handleImageUpload(req, res) {
                     'Content-Type': 'application/json'
                 },
                 body: JSON.stringify({
-                    model: 'llama-3.2-11b-vision-preview',
+                    model: 'meta-llama/llama-3.2-11b-vision-instruct',
                     messages: [
                         {
                             role: 'system',
